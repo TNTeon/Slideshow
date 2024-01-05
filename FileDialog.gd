72d1f1d4aaac61dddf1c1246be5209ec7ce48c18
TNTeon <39466405+TNTeon@users.noreply.github.com>
2024-01-04 23:03:59 -0600

    NotWorking

--- FileDialog.gd
+++ FileDialog.gd
@@ -13,11 +13,10 @@ func _on_file_dialog_dir_selected(dir):
 	for file in DirAccess.get_directories_at(dir):
 		openWindows(dir+"/"+file, numberOfFolders)
 		numberOfFolders += 1
+	$Timer.start()
 	
 	
 func openWindows(dir,displayCount):
-	var imageList = []
-	var imageNumber = 0
 	
 	await get_tree().create_timer(1.0).timeout
 	get_viewport().gui_embed_subwindows = false
@@ -27,31 +26,3 @@ func openWindows(dir,displayCount):
 	d.visible = true
 	d.position = Vector2(800,800)
 	d.size = Vector2(300,300)
-	d.get_window().current_screen = displayCount
-	
-	var displayRes = DisplayServer.screen_get_size(displayCount)
-	var directory = DirAccess.get_files_at(dir)
-	
-	for i in directory:
-		if ".png" in i:
-			imageList.append(i)
-	
-	dSprite.position = Vector2(displayRes.x/2,displayRes.y/2)
-	d.mode = 3
-	
-	$Timer.start()
-	while true:
-		var image = Image.new()
-		image.load(dir+"/"+imageList[imageNumber])
-	
-		var image_texture = ImageTexture.new()
-		image_texture.set_image(image)
-	
-		dSprite.texture = image_texture
-		
-		dSprite.position = Vector2(displayRes.x/2,displayRes.y/2)
-		dSprite.scale = Vector2(float(displayRes.x)/float(image_texture.get_width()),float(displayRes.y)/float(image_texture.get_height()))
-		
-		
-		await get_tree().create_timer(30.0).timeout
-		imageNumber +=1
